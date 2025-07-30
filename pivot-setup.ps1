8e65f78bd0b6a3355f6390d7ea312c2777abea79
lshoulders <lshoulders@pivotcharter.org>
2025-07-30 13:39:59 -0700

    First commit

---
+++ pivot-setup.ps1
@@ -0,0 +1,204 @@
+param (
+    [string]$site = "NB",
+    [switch]$force_update 
+)
+
+########################################################################################## Util functions ############################################################################################################
+
+function uninstall {
+    param ($app_name)
+
+    Write-Host "Searching for $app_name"
+    winget uninstall --all --accept-source-agreements $app_name
+}
+
+function install {
+    param ($app_name)
+
+    $curr_user = whoami.exe | Split-Path -Leaf
+
+    if ($curr_user.ToLower() -eq "admin") {
+        Write-Host "Updating the manifest for $app_name"
+        wingetcreate update $app_name
+    }
+    
+    Write-Host "Searching for $app_name"
+    winget install -e --accept-package-agreements $app_name
+}
+
+function free_bstr {
+    param ([System.IntPtr]$bstr)
+
+    if ($bstr -ne [IntPtr]::Zero) {
+        [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($bstr)
+    }
+}
+
+function compare_secure_string {
+    param (
+        [System.Security.SecureString]$SecStr1,
+        [System.Security.SecureString]$SecStr2
+    )
+
+    $bstr1 = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecStr1)
+    $bstr2 = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecStr2)
+
+
+    $len1 = [Runtime.InteropServices.Marshal]::ReadInt32($bstr1, -4)
+    $len2 = [Runtime.InteropServices.Marshal]::ReadInt32($bstr2, -4)
+
+    if ($len1 -ne $len2) {
+        free_bstr($bstr1)
+        free_bstr($bstr2)
+        return $false
+    }
+
+    for ($i = 0; $i -lt $len1; ++$i) {
+        $b1 = [Runtime.InteropServices.Marshal]::ReadByte($bstr1, $i)
+        $b2 = [Runtime.InteropServices.Marshal]::ReadByte($bstr2, $i)
+        if ($b1 -ne $b2) {
+            free_bstr($bstr1)
+            free_bstr($bstr2)
+            return $false
+        }
+    }
+
+    free_bstr($bstr1)
+    free_bstr($bstr2)
+    return $true
+}
+
+
+function get_password {
+    param ([string]$user)
+
+    $password1 = Read-Host -AsSecureString -Prompt "Enter password for $user"
+    $password2 = Read-Host -AsSecureString -Prompt "confirm password $user"
+
+    $match = compare_secure_string $password1 $password2
+    if (!$match) {
+        Write-Host "Passwords do not match! Try again." -BackgroundColor "red"
+        return get_password($user)
+    }
+    return $password1
+}
+
+function add_to_task_bar {
+    param (
+        $exe,
+        $lnk
+    )
+
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($lnk)
+    $Shortcut.TargetPath = $exe
+    $Shortcut.Save()
+
+    $Shell = New-Object -Verbose -ComObject Shell.Application
+    $Shell.NameSpace((Split-Path $lnk)).ParseName((Split-Path $lnk -Leaf)).InvokeVerb("PinToTaskbar")
+}
+
+########################################################################################## Start Script ############################################################################################################
+
+$status = Get-AppPackage *Microsoft.WindowsStore* | Select-Object status
+if (!($status.status -eq 'Ok')) {
+    Write-Host "Updating Microsoft store" -ForegroundColor "yellow" -BackgroundColor "blue"
+    Write-Host
+    Get-AppxPackage *WindowsStore* -AllUsers | ForEach-Object {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\\AppXManifest.xml"}
+}
+
+$min_winget = 1.11
+If ((Get-Command -Name "winget" -ErrorAction SilentlyContinue)) {
+    $winget_v = (winget -v).SubString(1)
+}
+else {
+    $winget_v = 0.0
+}
+
+If (($winget_v -lt $min_winget) -or $force_update) {
+    Write-Host "Installing winget" -ForegroundColor "yellow" -BackgroundColor "blue"    
+    Write-Host
+    # Install winget
+    Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile winget.msixbundle
+    Add-AppxPackage winget.msixbundle
+    Remove-Item winget.msixbundle
+    Install-Module -Name Microsoft.WinGet.Client
+}
+
+Write-Host
+Write-Host
+Write-Host "Removing Uneeded Apps" -ForegroundColor "yellow" -BackgroundColor "blue"
+Write-Host
+
+uninstall("Microsoft Copilot")
+uninstall("Microsoft 365 Copilot")
+uninstall("Microsoft Edge Browser")
+uninstall("Microsoft Edge DevTools Preview")
+uninstall("Microsoft Beta")
+uninstall("Microsoft Canary")
+uninstall("Microsoft Dev")
+uninstall("Microsoft WebDriver")
+uninstall("Microsoft WebView2 Runtime")
+
+Write-Host
+Write-Host
+Write-Host "Installing Packages" -ForegroundColor "yellow" -BackgroundColor "blue"
+Write-Host
+
+winget install wingetcreate
+
+install("Google.Chrome")
+
+Write-Host
+Write-Host
+Write-Host "Creating Shortcuts" -ForegroundColor "yellow" -BackgroundColor "blue"
+Write-Host
+$chrome_lnk = "Google Chrome.lnk"
+add_to_task_bar "C:\Program Files\Google\Chrome\Application\chrome.exe"  "$Home\Desktop\$chrome_lnk"
+
+Write-Host
+Write-Host
+Write-Host "Removing Shortcuts" -ForegroundColor "yellow" -BackgroundColor "blue"
+Write-Host
+$msedge_ln = "Microsoft Edge.lnk"
+Remove-Item -ErrorAction SilentlyContinue "$Home\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\$msedge_ln"
+Remove-Item -ErrorAction SilentlyContinue "$Home\Desktop\$msedge_ln"
+Remove-Item -ErrorAction SilentlyContinue "C:\Users\Public\Desktop\$msedge_ln"
+Remove-Item -ErrorAction SilentlyContinue "$Home\Desktop\$chrome_lnk"
+
+
+$curr_user = whoami.exe | Split-Path -Leaf
+if ($curr_user.ToLower() -eq "admin"){
+
+
+    Remove-LocalUser -ErrorAction SilentlyContinue -Name "User"
+    Remove-LocalUser -ErrorAction SilentlyContinue -Name "user"
+
+    $new_pass_curr_user = get_password($curr_user)
+    Set-LocalUser -Name $curr_user -Password $new_pass_curr_user
+
+    $user = "user"
+    Write-Host
+    Write-Host
+    Write-Host "Adding new default $user" -ForegroundColor "yellow" -BackgroundColor "blue"
+
+    $password = get_password($user)
+    New-LocalUser -Name $user -Password $password -FullName $user
+    Add-LocalGroupMember -Group "Users" -Member $user
+
+    .\printers.ps1 $site
+
+    $assetnbr = Read-Host -Prompt "Enter the pivot asset tag number"
+    $pcname = "pivot_$assetnbr"
+
+    Rename-Computer -NewName $pcname -Restart
+}
+
+Write-Host
+
+Get-WmiObject win32_bios
+Get-WmiObject win32_computersystem
+
+Read-Host
+
+shutdown.exe /s /t 10
---
+++ printers.ps1
@@ -0,0 +1,61 @@
+param (
+    [string]$site
+)
+
+if(($site.ToLower() -eq "nb") -or ($site.ToLower() -eq "north bay") -or ($site.ToLower() -eq "northbay")) {
+    Write-Host 
+    Write-Host "Setting up North Bay Printers" -BackgroundColor "blue"
+    Start-Process -Wait .\..\drivers\northbay_printer_setup.exe
+
+    
+    #Kyocera 3554ci driver printer(s)
+    $k3354ci_driver = "Kyocera TASKalfa 3554ci KX"
+    Add-PrinterDriver -Name $k3354ci_driver
+
+    Write-Host "Adding Middle School Printer" -BackgroundColor "green"
+    $ms_printer_ip = "10.11.10.200"
+    $ms_port_name = "IP_" + $ms_printer_ip + "_KX"
+    $ms_printer_name = "Middle School"
+    Remove-Printer -ErrorAction SilentlyContinue -Name $ms_printer_name
+    Add-PrinterPort -Name $ms_port_name -PrinterHostAddress $ms_printer_ip
+    Add-Printer -Name $ms_printer_name -PortName $ms_port_name -DriverName $k3354ci_driver
+
+    
+    #Kyocera 358ci driver priner(s)
+    $k358ci_driver = "Kyocera TASKalfa 358ci KX"
+    Add-PrinterDriver -Name $k358ci_driver
+
+    Write-Host "Adding Staff Area Printer" -BackgroundColor "green"
+    $staff_printer_ip = "10.11.10.202"
+    $staff_port_name = "IP_" + $staff_printer_ip + "_KX"
+    $staff_printer_name = "Staff Area"
+    Remove-Printer -ErrorAction SilentlyContinue -Name $staff_printer_name
+    Add-PrinterPort -Name $staff_port_name -PrinterHostAddress $staff_printer_ip
+    Add-Printer -Name $staff_printer_name -PortName $staff_port_name -DriverName $k358ci_driver
+
+    Write-Host "Adding Suite A Printer" -BackgroundColor "green"
+    $stea_printer_ip = "10.11.10.141"
+    $stea_port_name = "IP_" + $stea_printer_ip + "_KX"
+    $stea_printer_name = "Suite A"
+    Remove-Printer -ErrorAction SilentlyContinue -Name $stea_printer_name
+    Add-PrinterPort -Name $stea_port_name -PrinterHostAddress $stea_printer_ip
+    Add-Printer -Name $stea_printer_name -PortName $stea_port_name -DriverName $k358ci_driver
+}
+
+if(($site.ToLower() -eq "nv") -or ($site.ToLower() -eq "north valley") -or ($site.ToLower() -eq "northvalley")) {
+    Write-Host 
+    Write-Host "Setting up North Valley Printers" -BackgroundColor "blue"
+    pnputil.exe /add-driver ..\drivers\PCL6\KOBxxK__01.inf
+
+    #konica minolta printer
+    $km_driver = "KONICA MINOLTA Universal V4 PCL"
+    Add-PrinterDriver -Name $km_driver
+
+    Write-Host "Adding North Valley printer" -BackgroundColor "green"
+    $km_printer_ip = "192.169.1.150"
+    $km_port_name = "IP_" + $km_printer_ip
+    $km_printer_name = "North Valley"
+    Remove-Printer -ErrorAction SilentlyContinue -Name $km_port_name
+    Add-PrinterPort -Name $km_printer_name -PrinterHostAddress $km_printer_ip
+    Add-Printer -Name $km_port_name -PortName $km_printer_name -DriverName $k358ci_driver
+}
